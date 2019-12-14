5e873cc31978b40e1f2dcd46b087dbc56c247899
Christopher Melby <melby.christopher@gmail.com>
2019-12-14 13:28:35 -0800

    adding new file. Basic HTML layed out, js file created for initializing materialize features in HTML.

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+//Page load event listener for carousel.......................
+
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+
+
+
+
+
+
+
+
+
+
+
+  //Keep this at the Bottom. This initializes any built in materialize js that we set in place.............
+  M.AutoInit();
+
+
