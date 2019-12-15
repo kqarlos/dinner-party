50ae4338f5ba846337b30fb9dca611ed18841dbc
Christopher Melby <melby.christopher@gmail.com>
2019-12-15 13:06:27 -0800

    Updated notes on HTML file for what else needs to be done

--- script.js
+++ script.js
@@ -22,10 +22,22 @@ $('.fixed-action-btn').closeFAB();
 $('.fixed-action-btn.toolbar').openToolbar();
 $('.fixed-action-btn.toolbar').closeToolbar();
       
-
-
-
-
+//Scroll fire function to prompt user to select their main ingredient...........
+var options = [
+  {selector: '#staggered-test', offset: 50, callback: function(el) {
+    Materialize.toast("This is our ScrollFire Demo!", 1500 );
+  } },
+  {selector: '#staggered-test', offset: 205, callback: function(el) {
+    Materialize.toast("Please continue scrolling!", 1500 );
+  } },
+  {selector: '#staggered-test', offset: 400, callback: function(el) {
+    Materialize.showStaggeredList($(el));
+  } },
+  {selector: '#image-test', offset: 500, callback: function(el) {
+    Materialize.fadeInImage($(el));
+  } }
+];
+Materialize.scrollFire(options);
 
 
 
