db95a26cabf1fc815bb3f0c166599a03977d914c
Christopher Melby <melby.christopher@gmail.com>
2019-12-16 17:07:29 -0800

    pushing updates to HTML

--- script.js
+++ script.js
@@ -23,14 +23,10 @@ $('.fixed-action-btn.toolbar').openToolbar();
 $('.fixed-action-btn.toolbar').closeToolbar();
       
 //Scroll fire function to prompt user to select their main ingredient...........
+
+Materialize.scrollFire(options);
 var options = [
-  {selector: '#staggered-test', offset: 50, callback: function(el) {
-    Materialize.toast("This is our ScrollFire Demo!", 1500 );
-  } },
-  {selector: '#staggered-test', offset: 205, callback: function(el) {
-    Materialize.toast("Please continue scrolling!", 1500 );
-  } },
-  {selector: '#staggered-test', offset: 400, callback: function(el) {
+  {selector: '#staggered-test', offset: 20, callback: function(el) {
     Materialize.showStaggeredList($(el));
   } },
   {selector: '#image-test', offset: 500, callback: function(el) {
