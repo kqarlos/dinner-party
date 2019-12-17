2a77d237ed9d2f3f7f44175972c5d1d127a6eee0
Chengyu Ko <kokevin678@gmail.com>
2019-12-17 00:41:26 -0800

    add event listener from html to search API

--- food.js
+++ food.js
@@ -1,71 +1,60 @@
-$(document).ready(function(){
-
-    $(".card-image").click(function() {
+$(document).ready(function() {
+    var newDiv = $("<div class='row'>");
+    $(".card").click(function() {
+        var mainIng = $(this).attr("id");
         $(".row").remove();
-        $(".container").append();
-        return false;
-    // });
+        $(".newDiv").append();
+        console.log(mainIng);
 
-    var search = document.getElementById(click)
-    var queryURL = "https://api.edamam.com/search?q=" + search + "&app_id=a9502a10&app_key=38e9596cea1782797a3e09245c9370fb&from=0&to=100";
+    var food = mainIng    
+    var queryURL = "https://api.edamam.com/search?q=" + food + "&app_id=a9502a10&app_key=38e9596cea1782797a3e09245c9370fb&from=0&to=100";
     console.log(queryURL);
     
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-    console.log(response)
+        console.log(response);
+
+        var randomNo = [];
+        for (var i = 0; i < 6; i++) {
+            var random = Math.floor(Math.random() * 100);
+            var genNumber = randomNo.indexOf(random);
+            if (genNumber === -1) {
+                randomNo.push(random);
+            }}
     
-    var randomNo = [];
-    for (var i = 0; i < 6; i++) {
-        var random = Math.floor(Math.random() * 100);
-        var genNumber = randomNo.indexOf(random);
-        if (genNumber === -1) {
-            randomNo.push(random);
-        }
-
-        console.log(random);
-        var foodImg = response.hits[random].recipe.image;
-        var foodName = response.hits[random].recipe.label;
-        var recipe = response.hits[random].recipe.ingredientLines;
-
-
-        var container = $("<div class='col s4'>");
-        var foodContainer = $("<div class='card'>");
-        var foodImgDiv = $("<div class='card-image'>");
-        var foodImgEl = $("<img>");
-        foodImgEl.attr("src", foodImg);
-        var foodNameEl = $("<span class='card-title black-text'>");
-        var recipeEl = $("<p class='recipe'>");
-
-
-        foodNameEl.text(foodName);
-        foodImgDiv.append(foodImgEl, foodNameEl);
-        foodContainer.append(foodImgDiv);
-
-        container.append(foodContainer);
-
-        var detailContainer = $("div class='col s3 card'>");
-
-
-
-    
-    //this appens to test.html.
-    $("#test").append(container);
-
-    $(document).event
-    $(".card-image").click(function() {
-        $(".card").remove();
-        // $(".detailContainer").append();
-        return false;
-    });
+            console.log(random);
+            var foodImg = response.hits[random].recipe.image;
+            var foodName = response.hits[random].recipe.label;
+            var recipe = response.hits[random].recipe.ingredientLines;
     
     
-
+            var container = $("<div class='col s4'>");
+            var foodContainer = $("<div class='card'>");
+            var foodImgDiv = $("<div class='card-image'>");
+            var foodImgEl = $("<img>");
+            foodImgEl.attr("src", foodImg);
+            var foodNameEl = $("<span class='card-title black-text'>");
+            var recipeEl = $("<p class='recipe'>");
+    
+    
+            foodNameEl.text(foodName);
+            foodImgDiv.append(foodImgEl, foodNameEl);
+            foodContainer.append(foodImgDiv);
+    
+            container.append(foodContainer);
+    
+            var detailContainer = $("div class='col s3 card'>");
+    
+    
+    
         
-    
-    }
+        //this appens to test.html.
+        $(".newDiv").append(container);
+
     });
+
 
     });
 });
