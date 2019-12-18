e4307b63ed9dc7a521837f2b96222396c22c4bfc
kqarlos <kqarlos@hotmail.com>
2019-12-18 13:19:38 -0800

    Cleared up code

--- cocktail.js
+++ cocktail.js
@@ -1,20 +1,5 @@
-
-
-// //add this line to the listener when they click a dish choice this will generate the drinks card
-//------ getDrinkID(getCocktail);-----
-
 var spirit;
 var drinkID;
-
-//sets spirit once a protein is chosen
-// $("#features").on("click", ".card", function () {
-    // console.log($(this)[0].id);
-    // spirit = getSpirit($(this)[0].id);
-    // console.log("Spirit: " + spirit);
-    // $("#features").empty();
-    // getDrinkID(getCocktail);
-// });
-
 
 //based on a given protein figure out a spirit that pairs
 function getSpirit(protein) {
@@ -36,18 +21,15 @@ function getSpirit(protein) {
 
 //get drinkId based on spirit and API. Unpon completion call callback function
 function getDrinkID(callback) {
-    console.log("Spirit queried: " + spirit);
     //building query to get a drink
     var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log("getDrinkID query");
-        console.log(queryURL);
+        console.log("Drink query: " + queryURL);
         console.log(response);
         var randomDrink = Math.floor(Math.random() * response.drinks.length);
-        // console.log("Random drink index:" + randomDrink);
         drinkID = response.drinks[randomDrink].idDrink;
         console.log("drinkID: " + drinkID);
         callback();
@@ -57,24 +39,16 @@ function getDrinkID(callback) {
 //Given a drink ID get the cocktail information. Call to render cocktail pnce information is completely retireved
 function getCocktail() {
 
-    console.log("Random drink id queried: " + drinkID);
-
     //query building to lookup cocktail info
-
-    // var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=san&francisco"
     var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkID;
-
-    //execute query
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log("getCocktail query");
-        console.log(queryURL);
+        console.log("Get cocktail based ID query: " + queryURL);
         console.log(response);
 
         var drinkInfo = response.drinks[0];
-
         renderCocktail(drinkInfo);
     });
 }
@@ -82,40 +56,33 @@ function getCocktail() {
 //render cocktail to html
 function renderCocktail(drinkInfo) {
 
-    // //setting up row column and card
-    // var row = $("<div>");
-    // row.addClass("row justify-content-center");
-
+    //Creating column to append to #choices row
     var column = $("<div>");
     column.addClass("col s6 m6");
 
+    //create card
     var card = $("<div>");
     card.addClass("card z-depth-4");
-
-
     //set up image with title
     var cardImg = $("<div>");
     cardImg.addClass("card-image");
-
-
     var img = $("<img>");
     img.attr("src", drinkInfo.strDrinkThumb);
-
+    //set up title
     var titleSpan = $("<span>");
     titleSpan.addClass("card-title");
-
     var title = $("<h3>");
     title.text(drinkInfo.strDrink);
-
     titleSpan.append(title);
     cardImg.append(img);
     cardImg.append(titleSpan);
-    //done setting up image 
+    //done setting up image  wit title
 
     //setting up content
     var content = $("<div>");
     content.addClass("card-content");
 
+    //Recipe and ingredients ---------
     var ingredientList = $("<ol>");
     ingredientList.text("Ingredients");
 
@@ -137,23 +104,15 @@ function renderCocktail(drinkInfo) {
     var ingredient = $("<li>");
     ingredient.text(drinkInfo.strGlass);
     ingredientList.append(ingredient);
-
     var recipe = $("<p>");
     recipe.text("Recipe: " + drinkInfo.strInstructions);
 
     content.append(ingredientList);
     content.append(recipe);
+    //done setting up content / append to card
 
-    //done setting up content
     card.append(cardImg);
     card.append(content);
-
     column.append(card);
-
-    //------append column to row----
-    // $(row).append(column);
-
-
-
     $("#choices").append(column);
 }
--- food.js
+++ food.js
@@ -1,155 +1,57 @@
-var protein = "";
+var mainIng = "";
+
 
 $(document).ready(function () {
 
     $(".card").click(function () {
 
+        //set main ingredient
+        mainIng = $(this).attr("id");
+        console.log("Main Ingredient: " + mainIng);
 
-        spirit = getSpirit($(this)[0].id);
+        //set spirit for cocktail based on main ingredient
+        spirit = getSpirit(mainIng);
+        console.log("Spirit: " + spirit);
 
-
-        var mainIng = $(this).attr("id");
-        protein = mainIng;
+        //clear #features div and create row to be populated
         $("#features").empty();
-
-        // $(".row").append(".newDiv");
-        console.log(mainIng);
-
         var row = $("<div>");
         row.addClass("row justify-content-center");
         row.attr("id", "choices");
         $("#features").append(row);
 
-        var food = mainIng
-        var queryURL = "https://api.edamam.com/search?q=" + food + "&app_id=a9502a10&app_key=38e9596cea1782797a3e09245c9370fb&from=0&to=100";
-        console.log(queryURL);
-
+        //Preparing query for food dishes based on mainIng
+        var queryURL = "https://api.edamam.com/search?q=" + mainIng + "&app_id=a9502a10&app_key=38e9596cea1782797a3e09245c9370fb&from=0&to=100";
+        console.log("FOOD list query: " + queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
 
+            //Generate random numbers to use as indexes from query result
             var randomNo = [];
             for (var i = 0; i < 6; i++) {
                 var random = Math.floor(Math.random() * 100);
                 var genNumber = randomNo.indexOf(random);
                 if (genNumber === -1) {
-                randomNo.push(random);
+                    randomNo.push(random);
                 }
                 else {
                     i--;
                 }
             }
 
-            console.log(randomNo);
+            console.log("Random numbers: " + randomNo);
 
+            //Get dishes using random number and get their information
             for (var i = 0; randomNo.length; i++) {
                 var foodImg = response.hits[randomNo[i]].recipe.image;
                 var foodName = response.hits[randomNo[i]].recipe.label;
-                var recipe = response.hits[randomNo[i]].recipe.ingredientLines;
+                // var ingredients = response.hits[randomNo[i]].recipe.ingredientLines;
+                var ingredients = "";
 
-                // var newDiv = $("<div class='row'>");
-
-                // var container = $("<div class='col s6 m6'>");
-                // var foodContainer = $("<div class='card z-depth-4'>");
-                // var foodImgDiv = $("<div class='card-image'>");
-                // var foodImgEl = $("<img>");
-                // foodImgEl.attr("src", foodImg);
-                // var foodNameEl = $("<span class='card-title black-text'>");
-                // var recipeEl = $("<p class='recipe'>");
-
-
-                // foodNameEl.text(foodName);
-                // foodImgDiv.append(foodImgEl, foodNameEl);
-                // foodContainer.append(foodImgDiv);
-
-                // container.append(foodContainer);
-
-                // var detailContainer = $("div class='col s3 card'>");
-
-
-
-
-                // //this appens to test.html.
-                // $(".newDiv").append(container);
-                // $("#features").append(newDiv);
-
-
-                //setting up row column and card
-                // var row = $("<div>");
-                // row.addClass("row justify-content-center");
-                var column = $("<div>");
-                column.addClass("col s12 m6");
-                var card = $("<div>");
-                card.addClass("choices card z-depth-4");
-
-                //addCardInfo
-                card.attr("data-number", randomNo[i]);
-
-                //set up image with title
-                var cardImg = $("<div>");
-                cardImg.addClass("card-image");
-                var img = $("<img>");
-                img.attr("src", foodImg);
-                var titleSpan = $("<span>");
-                titleSpan.addClass("card-title");
-                var title = $("<h3>");
-                title.text(foodName);
-                titleSpan.append(title);
-                cardImg.append(img);
-                cardImg.append(titleSpan);
-
-                var aTag = $("<a>");
-                aTag.addClass("btn-floating btn-large btn waves-effect waves-red  halfway-fab cyan pulse");
-                var iTag = $("<i>");
-                iTag.addClass("material-icons");
-                iTag.text("add");
-
-                aTag.append(iTag);
-                cardImg.append(aTag);
-                //<a class="btn-floating btn-large btn waves-effect waves-red  halfway-fab cyan pulse"><i class="material-icons">add</i></a>
-
-
-                //done setting up image 
-                //setting up content
-                var content = $("<div>");
-                content.addClass("card-content");
-
-                // var ingredientList = $("<ol>");
-                // ingredientList.text("Ingredients");
-                // for (var i = 1; i <= 15; i++) {
-                //     var measure = eval("drinkInfo.strMeasure" + i);
-                //     var name = eval("drinkInfo.strIngredient" + i);
-                //     console.log(measure + " " + name);
-                //     if (!measure)
-                //         measure = "";
-                //     if (name) {
-                //         var ingredient = $("<li>");
-                //         ingredient.text(measure + " " + name);
-                //         ingredientList.append(ingredient);
-                //     }
-                // }
-                // var ingredient = $("<li>");
-                // ingredient.text(drinkInfo.strGlass);
-                // ingredientList.append(ingredient);
-
-
-                var recipe = $("<p>");
-                // recipe.text("Recipe: " + recipe);
-                recipe.text("");
-
-                // content.append(ingredientList);
-                content.append(recipe);
-                //done setting up content
-                card.append(cardImg);
-                card.append(content);
-                column.append(card);
-
-                //------append column to row----
-                $(row).append(column);
-                // $("#features").append(row);
-
+                renderFood(foodImg, foodName, ingredients, randomNo[i]);
             }
 
 
@@ -159,134 +61,83 @@ $(document).ready(function () {
     });
 });
 
+//Event listener to when a dish selection has been made
 $(document).on("click", ".choices", function () {
-    console.log($(this));
+    //Clear row
     $("#choices").empty();
-    getDrinkID(getCocktail);
 
+    //Dish number chosen from list
     var number = $(this).attr("data-number");
-    console.log("number: ");
-
-    console.log(number);
-
-
 
     // --------------------------------------ADD FOOD CARD---------------------
-    var queryURL = "https://api.edamam.com/search?q=" + protein + "&app_id=a9502a10&app_key=38e9596cea1782797a3e09245c9370fb&from=0&to=100";
-    console.log(queryURL);
+    var queryURL = "https://api.edamam.com/search?q=" + mainIng + "&app_id=a9502a10&app_key=38e9596cea1782797a3e09245c9370fb&from=0&to=100";
+    console.log("Dish query: " + queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log("RESPONSE FOR FOOD CARD HERE!!");
         console.log(response);
-
-
-
-        // console.log(randomNo);
 
         var foodImg = response.hits[number].recipe.image;
         var foodName = response.hits[number].recipe.label;
-        var recipeText = response.hits[number].recipe.ingredientLines;
+        var ingredients = response.hits[number].recipe.ingredientLines;
 
-        // var newDiv = $("<div class='row'>");
-
-        // var container = $("<div class='col s6 m6'>");
-        // var foodContainer = $("<div class='card z-depth-4'>");
-        // var foodImgDiv = $("<div class='card-image'>");
-        // var foodImgEl = $("<img>");
-        // foodImgEl.attr("src", foodImg);
-        // var foodNameEl = $("<span class='card-title black-text'>");
-        // var recipeEl = $("<p class='recipe'>");
-
-
-        // foodNameEl.text(foodName);
-        // foodImgDiv.append(foodImgEl, foodNameEl);
-        // foodContainer.append(foodImgDiv);
-
-        // container.append(foodContainer);
-
-        // var detailContainer = $("div class='col s3 card'>");
-
-
-
-
-        // //this appens to test.html.
-        // $(".newDiv").append(container);
-        // $("#features").append(newDiv);
-
-
-        //setting up row column and card
-        // var row = $("<div>");
-        // row.addClass("row justify-content-center");
-        var column = $("<div>");
-        column.addClass("col s6");
-        var card = $("<div>");
-        card.addClass("choices card z-depth-4");
-
-        //addCardInfo
-        // card.attr("data-number", randomNo[i]);
-
-        //set up image with title
-        var cardImg = $("<div>");
-        cardImg.addClass("card-image");
-        var img = $("<img>");
-        img.attr("src", foodImg);
-        var titleSpan = $("<span>");
-        titleSpan.addClass("card-title");
-        var title = $("<h3>");
-        title.text(foodName);
-        titleSpan.append(title);
-        cardImg.append(img);
-        cardImg.append(titleSpan);
-        //done setting up image 
-        //setting up content
-        var content = $("<div>");
-        content.addClass("card-content");
-
-        // var ingredientList = $("<ol>");
-        // ingredientList.text("Ingredients");
-        // for (var i = 1; i <= 15; i++) {
-        //     var measure = eval("drinkInfo.strMeasure" + i);
-        //     var name = eval("drinkInfo.strIngredient" + i);
-        //     console.log(measure + " " + name);
-        //     if (!measure)
-        //         measure = "";
-        //     if (name) {
-        //         var ingredient = $("<li>");
-        //         ingredient.text(measure + " " + name);
-        //         ingredientList.append(ingredient);
-        //     }
-        // }
-        // var ingredient = $("<li>");
-        // ingredient.text(drinkInfo.strGlass);
-        // ingredientList.append(ingredient);
-
-
-        var recipe = $("<p>");
-        recipe.text("Ingredietns: " + recipeText);
-        // recipe.text("");
-
-        // content.append(ingredientList);
-        content.append(recipe);
-        //done setting up content
-        card.append(cardImg);
-        card.append(content);
-        column.append(card);
-
-        //------append column to row----
-        $("#choices").append(column);
-        // $("#features").append(row);
-
-
-
-
+        renderFood(foodImg, foodName, ingredients, number);
     });
 
-    // --------------------------------------ADD FOOD CARD---------------------
-
-
-
-
-
+    //Call to cocktail.js to query and generate cocktail
+    getDrinkID(getCocktail);
 });
+
+//Renders food card with given information
+function renderFood(foodImg, foodName, ingredients, dishNumber) {
+    //setting up column and cards to add to the row
+    var column = $("<div>");
+    column.addClass("col s12 m6");
+    var card = $("<div>");
+    card.addClass("choices card z-depth-4");
+
+    //Create dish card
+    card.attr("data-number", dishNumber);
+
+    //set up image with title and button---------
+    var cardImg = $("<div>");
+    cardImg.addClass("card-image");
+    var img = $("<img>");
+    img.attr("src", foodImg);
+    //setting up title
+    var titleSpan = $("<span>");
+    titleSpan.addClass("card-title");
+    var title = $("<h3>");
+    title.text(foodName);
+    titleSpan.append(title);
+    cardImg.append(img);
+    cardImg.append(titleSpan);
+    //setting up button only if dishes are choices => ingredients string is empty
+    if (ingredients === "") {
+        var aTag = $("<a>");
+        aTag.addClass("btn-floating btn-large btn waves-effect waves-red  halfway-fab cyan pulse");
+        var iTag = $("<i>");
+        iTag.addClass("material-icons");
+        iTag.text("add");
+        aTag.append(iTag);
+        // ----------------------------------------
+        cardImg.append(aTag);
+    }
+    //done setting up image -------------------
+
+    //setting up content-----------------------
+    var content = $("<div>");
+    content.addClass("card-content");
+
+    var recipe = $("<p>");
+    recipe.text(ingredients);
+    content.append(recipe);
+
+    //done setting up content / Append everythin to row
+
+    card.append(cardImg);
+    card.append(content);
+    column.append(card);
+    $("#choices").append(column);
+}
